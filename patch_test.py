17ede19acf17540332ba8cf051be0856155bbc75
tomer <tom7ly@gmail.com>
2021-09-27 10:49:05 +0300

    a

--- patch_test.py
+++ patch_test.py
@@ -1,9 +1,9 @@
-import numpy as np
+# import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-import tensorflow_addons as tfa
-from tensorflow.keras.layers.experimental.preprocessing import *
+# from tensorflow.keras import layers
+# import tensorflow_addons as tfa
+# from tensorflow.keras.layers.experimental.preprocessing import *
 import matplotlib.pyplot as plt
 
 
@@ -11,17 +11,20 @@ def crop_with_direction(data, left=0, right=0, bottom=0, top=0):
     # if(sum([left, right, bottom, top]) != 1):
     #     print("more than 1 direction was given")
     #     return "ERROR"
+    if(bottom == 1 or right == 1):
+        cropped_data = tf.image.crop_to_bounding_box(data, 0, 0, 32-bottom, 32-right)
+    else:
+        cropped_data = tf.image.crop_to_bounding_box(data, top, left, 32-top, 32-left)
 
-    cropped_data = tf.image.crop_to_bounding_box(data, top, left, 32-bottom, 32-right)
     padded_data = tf.image.pad_to_bounding_box(cropped_data, top, left, 32, 32)
     plt.imshow(padded_data)
     return padded_data
 
 
-# (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
 
-# img = x_train[0]
+img = x_train[0]
 
-# prcsed=crop_with_direction(img,1,0,0,0)
+prcsed=crop_with_direction(img,1,0,0,0)
 
 print(1)
